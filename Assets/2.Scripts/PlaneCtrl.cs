ab21211ffa944d15aef47238157b744f88cba5e9
doy20n <129262545+doy20n@users.noreply.github.com>
2023-05-03 16:56:38 +0900

    one plane

---
+++ Assets/2.Scripts/PlaneCtrl.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaneCtrl : MonoBehaviour
+{
+
+   // public ARPlaneManager planeManager;
+   // public Button turnoff;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    private void TurnOff()
+    {
+        //planeManager.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
