baafe7b754d54b7dece360bc266f663b66cec552
doy20n <jdy00930@gmail.com>
2023-05-04 22:05:19 +0900

    Disable AR Plane Detection Button

--- Assets/2.Scripts/PlaneCtrl.cs
+++ Assets/2.Scripts/PlaneCtrl.cs
@@ -2,27 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.XR.ARFoundation;
 
 public class PlaneCtrl : MonoBehaviour
 {
 
-   // public ARPlaneManager planeManager;
-   // public Button turnoff;
+    private ARPlaneManager planeManager;
+    public Button turnoff;
 
 
     // Start is called before the first frame update
+    void Awake()
+    {
+        planeManager = GetComponent<ARPlaneManager>();
+    }
+    
     void Start()
     {
-
+        
     }
-    private void TurnOff()
+    public void TurnOff()
     {
-        //planeManager.enabled = false;
+        planeManager.enabled = false;
+
+        foreach(ARPlane plane in planeManager.trackables)
+        {
+            plane.gameObject.SetActive(planeManager.enabled);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
 }
