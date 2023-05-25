9898f7746e1d0bcc3108a1efec1785f10ab14d66
park7628 <pkm7628@gmail.com>
2023-05-25 16:05:14 +0900

    Lab UI Update

---
+++ Assets/2.Scripts/SettingCtrl.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ECountrolType
+{
+    Mouse,
+    KeybordMouse,
+    Touch
+}
+
+public class SettingCtrl
+{
+    public static ECountrolType controlType;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
