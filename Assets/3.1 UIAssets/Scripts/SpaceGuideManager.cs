84595c8d38081c137686a4ebd5f96ef7f8b87b85
doy20n <jdy00930@gmail.com>
2023-10-03 19:19:08 +0900

    Change Earth pos

---
+++ Assets/3.1 UIAssets/Scripts/SpaceGuideManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceGuideManager : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void Gtext6()
+    {
+
+    }
+    public void Gtext7()
+    {
+
+    }
+    public void Gtext8()
+    {
+
+    }
+    public void Gtext9()
+    {
+
+    }
+
+}
